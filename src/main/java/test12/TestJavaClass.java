c49ee030d29e2b26e6be8a310b3d3d705f107ea2
padhle <padhlee@gmail.com>
2021-02-26 22:24:36 +0530

    commit new project of jenkin

---
+++ src/main/java/test12/TestJavaClass.java
@@ -0,0 +1,11 @@
+package test12;
+
+public class TestJavaClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is Java");
+
+	}
+
+}
---
+++ src/test/java/test12/Testcase.java
@@ -0,0 +1,33 @@
+package test12;
+
+
+import org.junit.Test;
+
+public class Testcase {
+
+	// Test case 1
+			@Test
+			public void TestCase1() {
+			
+				System.out.println("Test case 1 run successfully");
+			//	throw new NullPointerException("Will fail");
+			
+			}
+			
+			// Test case 1
+			@Test
+			public void TestCase2() {
+			
+				System.out.println("Test case 2 run successfully");
+			
+			}
+			
+			// Test case 1
+			@Test
+			public void TestCase3() {
+			
+				System.out.println("Test case 3 run successfully");
+			
+			}
+
+}
